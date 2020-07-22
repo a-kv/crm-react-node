c702264a1cf4c13d861a0bc00d9a1c75f4868c91
kvaso <aakvasovaa@gmail.com>
2020-07-22 20:11:53 +0300

    Updates

---
+++ precache-manifest.9918a56f4f04127c8eb0c6e657600d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0c268ec464ece095ec789edf5479a2",
+    "url": "/crm-react-node/index.html"
+  },
+  {
+    "revision": "deefa845a2fd40a2391c",
+    "url": "/crm-react-node/static/css/main.dec843b9.chunk.css"
+  },
+  {
+    "revision": "f86096a21308eb04499e",
+    "url": "/crm-react-node/static/js/2.db96827f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crm-react-node/static/js/2.db96827f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deefa845a2fd40a2391c",
+    "url": "/crm-react-node/static/js/main.85c960c1.chunk.js"
+  },
+  {
+    "revision": "8b388dd668b74308b694",
+    "url": "/crm-react-node/static/js/runtime-main.a3920f64.js"
+  }
+]);
